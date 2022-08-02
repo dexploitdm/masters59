d7c3ccb5d3cf8e848fb083c603c2dd7d66394340
d.parshakov <d.parshakov@platiza.ru>
2022-08-02 13:08:48 +0500

    add language Ru

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'ru-RU',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
