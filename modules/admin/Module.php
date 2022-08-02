12250bde342d0c5363399d1b87c4431e598129ed
d.parshakov <d.parshakov@platiza.ru>
2022-08-02 14:27:59 +0500

    hide admin

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -16,27 +16,27 @@ class Module extends \yii\base\Module
      */
     public $controllerNamespace = 'app\modules\admin\controllers';
 
-//    public function behaviors()
-//    {
-//        return [
-//            'access'    =>  [
-//                'class' =>  AccessControl::className(),
-//                'denyCallback'  =>  function($rule, $action)
-//                {
-//                    throw new \yii\web\NotFoundHttpException();
-//                },
-//                'rules' =>  [
-//                    [
-//                        'allow' =>  true,
-//                        'matchCallback' =>  function($rule, $action)
-//                        {
-//                            return Yii::$app->user->identity->isAdmin;
-//                        }
-//                    ]
-//                ]
-//            ]
-//        ];
-//    }
+    public function behaviors()
+    {
+        return [
+            'access'    =>  [
+                'class' =>  AccessControl::className(),
+                'denyCallback'  =>  function($rule, $action)
+                {
+                    throw new \yii\web\NotFoundHttpException();
+                },
+                'rules' =>  [
+                    [
+                        'allow' =>  true,
+                        'matchCallback' =>  function($rule, $action)
+                        {
+                            return Yii::$app->user->identity->isAdmin;
+                        }
+                    ]
+                ]
+            ]
+        ];
+    }
 
     /**
      * @inheritdoc
